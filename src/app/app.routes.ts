13b2ed76d7de48c89f582d5301b23742f1375541
Corentin Lemaire <corentin.lemr@gmail.com>
2026-01-03 19:01:42 +0100

    :sparkles: Set up master + player pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PlayerComponent } from './player/player.component';
+import { MasterComponent } from './master/master.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PlayerComponent
+  },
+  {
+    path: 'master',
+    component: MasterComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
